f629fa4c21aa18fb775f4b6d0f0d979ab52f7ed8
Payal Ahirwar <payalahirwar03@gmail.com>
2023-07-27 13:06:05 +0530

    Fix line endings

---
+++ app.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path'); // Import the 'path' module
+ const homeRouter = require('./routers/homeRouter')
+
+
+// const Router = express.Router();
+const port  = process.env.port || 8000;
+
+const app  = express();
+
+// db con
+
+mongoose.connect('mongodb://127.0.0.1:27017/studentsdata',{useNewUrlParser:true})
+const db = mongoose.connection;
+
+db.on("error",()=>{console.log("error in conection");})
+db.once('open',()=>{console.log("Connected");})
+
+// // Set up the model for the schedule
+// const Schedule = mongoose.model('Schedule', {
+//     room: String,
+//     schedule: String,
+//   });
+
+//   const schedulesData = [
+//     { room: 'GS3', schedule: 'Some schedule data for GS3' },
+//     { room: 'GS4', schedule: 'Some schedule data for GS4' },
+//     // Add other schedule data objects here...
+//   ];
+
+
+//   Router.get('/Tools/Edit.ejs', (req, res) => {
+//     res.render('Tools/Edit', { schedules: schedulesData });
+//   });
+
+// Set the path to the 'views' folder
+// const viewsPath = path.join(__dirname, 'RegisterLogin', 'views');
+
+// Function to fetch schedules data from the database
+// async function fetchedSchedules() {
+//     try {
+//       const schedules = await Schedule.find(); // Fetch all schedules from the database
+//       return schedules;
+//     } catch (err) {
+//       console.error('Error fetching schedules from the database:', err);
+//       return []; // Return an empty array or handle the error appropriately
+//     }
+// }
+
+// app.set('views', viewsPath);
+app.set('view engine','ejs')
+
+app.get('/homeFaculty.ejs', (req, res) => {
+    res.render('homeFaculty'); // This will look for home.ejs in the views folder
+})
+
+app.get('/homeStudent.ejs', (req, res) => {
+    res.render('homeStudent'); // This will look for home.ejs in the views folder
+})
+
+ app.get('/hello.ejs', (req, res) => {
+     res.render('hello'); // This will look for home.ejs in the views folder
+ })
+
+//  app.get('/Tools/StudentView.ejs', (req, res) => {
+// //     res.render('Tools/StudentView'); // This will look for home.ejs in the views folder
+//  })
+
+// app.get('/Tools/window.ejs', (req, res) => {
+//     res.render('Tools/window'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/function.js', (req, res) => {
+//     res.render('Tools/function'); // This will look for home.ejs in the views folder
+// })
+
+//  app.get('/Tools/Edit.ejs', (req, res) => {
+//      res.render('Tools/Edit'); // This will look for home.ejs in the views folder
+//  })
+
+// app.get('/Tools/Faculty_CR/GS3.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/GS3'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Faculty_CR/GS4.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/GS4'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Faculty_CR/GS5.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/GS5'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Faculty_CR/GS6.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/GS6'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Faculty_CR/NLH1.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/NLH1'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Faculty_CR/NLH2.ejs', (req, res) => {
+//     res.render('Tools/Faculty_CR/NLH2'); // This will look for home.ejs in the views folder
+// })
+
+// app.get('/Tools/Student/GS3.ejs', (req, res) => {
+//     res.render('Tools/Student/GS3'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Student/GS4.ejs', (req, res) => {
+//     res.render('Tools/Student/GS4')
+// })
+
+// app.get('/Tools/Student/GS5.ejs', (req, res) => {
+//     res.render('Tools/Student/GS5'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Student/GS6.ejs', (req, res) => {
+//     res.render('Tools/Student/GS6'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Student/NLH1.ejs', (req, res) => {
+//     res.render('Tools/Student/NLH1'); // This will look for home.ejs in the views folder
+// })
+// app.get('/Tools/Student/NLH2.ejs', (req, res) => {
+//     res.render('Tools/Student/NLH2'); // This will look for home.ejs in the views folder
+// })
+
+// // Handle the GET request for the edit page
+// app.get('/Tools/Edit.ejs', (req, res) => {
+//     // Fetch the schedule data from the database and pass it to the edit.ejs template
+//     Schedule.find({}, (err, schedules) => {
+//       if (err) {
+//         console.error('Error fetching schedule data from the database:', err);
+//         res.status(500).send('An error occurred. Please try again later.');
+//       } else {
+//         res.render('Tools/Edit', { schedules });
+//       }
+//     });
+//   });
+  
+//   // Handle the POST request to save the edited schedule
+//   app.post('/Tools/Edit.ejs', (req, res) => {
+//     // Extract the schedule data from the request body
+//     const { room, schedule } = req.body;
+  
+//     // Find the schedule entry in the database and update it
+//     Schedule.findOneAndUpdate({ room }, { schedule }, { new: true, upsert: true }, (err, updatedSchedule) => {
+//       if (err) {
+//         console.error('Error updating schedule data in the database:', err);
+//         res.status(500).send('An error occurred. Please try again later.');
+//       } else {
+//         // Redirect to the edit page to show the updated schedule
+//         res.redirect('Tools/Edit');
+//       }
+//     });
+//   });
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/Tools/AddSchedule', homeRouter)
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/', homeRouter)
+
+app.listen(port)
+// module.exports = Router;
---
+++ routers/homeRouter.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const Router = express.Router();
+const mongoose = require('mongoose');
+const homeSchema = require('../models/homeSchema');
+// const Schedule = require('./routers/homeRouter');
+
+// Render the registration page
+Router.get('/', (req, res) => {
+  res.render('register', { title: 'Fill Form', password: '', email: '', error: '' });
+});
+
+// Handle user registration
+Router.post('/register', async (req, res) => {
+  try {
+    const { name, number, email, password, cpassword } = req.body;
+
+    if (password === cpassword) {
+      const user = await homeSchema.findOne({ email: email });
+
+      if (user) {
+        // User is already registered
+        return res.render('register', { title: '', password: '', email: '', error: 'You are already registered' });
+      }
+
+      // User is not registered, save the data
+      const userData = new homeSchema({
+        name,
+        number,
+        email,
+        password,
+      });
+
+      await userData.save();
+      return res.render('register', { title: 'Registered successfully', password: '', email: '', error: '' });
+    } else {
+      // Passwords do not match
+      return res.render('register', { title: '', password: 'Password not matching', email: '', error: '' });
+    }
+  } catch (error) {
+    return res.render('register', { title: 'Error in Code', password: '', email: '', error: 'An error occurred. Please try again.' });
+  }
+});
+
+// User login
+Router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await homeSchema.findOne({ email: email });
+
+    if (user) {
+      if (password === user.password) {
+        // Successful login
+        return res.render('Mainpage', {});
+      } else {
+        // Invalid password
+        return res.render('register', { title: '', password: '', email: '', error: 'Invalid password' });
+      }
+    } else {
+      // User not found
+      return res.render('register', { title: '', password: '', email: '', error: 'User not found. Please register.' });
+    }
+  } catch (error) {
+    return res.render('register', { title: '', password: '', email: '', error: 'An error occurred. Please try again.' });
+  }
+});
+
+
+const Schedule = mongoose.model('Schedule', {
+     serialNo:String,
+     section:String,
+    room: String,
+    // schedule: String,
+    subject: String,
+    time: String
+});
+
+
+// Handle the GET request for rendering the edit page and handling the POST request to save the edited schedule
+Router.route('/Tools/Edit.ejs')
+  .get(async (req, res) => {
+    try {
+      // Fetch the schedule data from the database and pass it to the edit.ejs template
+      const schedules = await Schedule.find({});
+      res.render('Tools/Edit', { schedules });
+      console.log(schedules);
+    } catch (err) {
+      console.error('Error fetching schedule data from the database:', err);
+      res.status(500).send('An error occurred. Please try again later.');
+    }
+  })
+  .post(express.json(), async (req, res) => {
+    // Extract the updated schedule data from the request body
+    const { schedules } = req.body;
+    console.log(schedules);
+
+    try {
+      // Loop through the updated schedules and update the database
+      for (const { serialNo,section, room, subject, time } of schedules) {
+        await Schedule.findOneAndUpdate(
+          {serialNo: serialNo},
+          {
+            $set: {
+              section,
+              room,
+              subject,
+              time,
+            },
+          },
+          { new: true }
+        );
+      }
+      console.log(schedules);
+      // Respond with a success message
+      res.json({ message: 'Schedule data updated successfully' });
+    } catch (err) {
+      console.error('Error updating schedule data in the database:', err);
+      // Send an error response
+      res.status(500).json({ error: 'An error occurred while saving the schedule. Please try again.' });
+    }
+  });
+
+
+
+// Handle the GET request for the student view of the schedule
+Router.get('/Tools/StudentView.ejs', async (req, res) => {
+  try {
+    // Fetch the schedule data from the database and pass it to the StudentView.ejs template
+    const schedules = await Schedule.find({});
+    res.render('Tools/StudentView', { schedules });
+  } catch (err) {
+    console.error('Error fetching schedule data from the database:', err);
+    res.status(500).send('An error occurred. Please try again later.');
+  }
+});
+
+// Handle the POST request to add a new schedule
+Router.post('/Tools/AddSchedule', express.json(), async (req, res) => {
+  // Extract the new schedule data from the request body
+  const { serialNo,section,room, subject, time } = req.body;
+
+  try {
+    // Create a new schedule document and save it to the database
+    const newSchedule = new Schedule({ serialNo,section,room, subject, time });
+    await newSchedule.save();
+
+    // Respond with a success message
+     // Respond with a success message
+  //  res.redirect('/Tools/Edit.ejs'); // Redirect to a confirmation page after successful addition
+    res.json({ message: 'New schedule added successfully' });
+  } catch (err) {
+    console.error('Error adding new schedule to the database:', err);
+    // Send an error response
+    res.status(500).json({ error: 'An error occurred while adding the schedule. Please try again.' });
+  }
+});
+
+// Handle the DELETE request to delete a schedule
+Router.delete('/Tools/DeleteSchedule/:id', async (req, res) => {
+  const scheduleId = req.params.id;
+
+  try {
+    // Find the schedule in the database and delete it
+    await Schedule.findByIdAndDelete(scheduleId);
+
+    // Respond with a success message
+    res.json({ message: 'Schedule deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting schedule from the database:', err);
+    // Send an error response
+    res.status(500).json({ error: 'An error occurred while deleting the schedule. Please try again.' });
+  }
+});
+
+module.exports =  Router ;
